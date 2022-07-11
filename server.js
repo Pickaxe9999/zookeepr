f2593be341249465731fb24bcba4bb176e681efb
Jacob H <jacob.hannah164@gmail.com>
2022-07-11 14:09:56 -0400

    animals page can now add animal objects to animals.json through api calls. Animals are also now displayed to animals.html

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray){
     let personalityTraitsArray = [];
 
@@ -87,6 +89,22 @@ app.get('/api/animals', (req, res) => {
 app.get('/api/animals/:id', (req,res) => {
     const result = findById(req.params.id, animals);
     res.json(result);
+})
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/animals', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+})
+
+app.get('/zookeepers', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
 app.post('/api/animals', (req,res) => {
